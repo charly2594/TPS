4882d8b3bc758cf116d563a091192fbcc7da4266
charly2594 <antinopai2594@gmail.com>
2019-08-19 13:14:46 -0700

    Added .travis.yaml config file for travis CI

--- src/test/java/io/tps/yugioh/cardcatalogservice/CardCatalogServiceApplicationTests.java
+++ src/test/java/io/tps/yugioh/cardcatalogservice/CardCatalogServiceApplicationTests.java
@@ -1,5 +1,7 @@
 package io.tps.yugioh.cardcatalogservice;
 
+import io.tps.yugioh.cardcatalogservice.models.CatalogCard;
+import io.tps.yugioh.cardcatalogservice.resources.CardCatalogResource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +12,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class CardCatalogServiceApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception{
+        /*
+        CardCatalogResource cardCatalogResource = null;
+        CatalogCard catalogCard = cardCatalogResource.cardSearch("Dark Magician");
+        System.out.println(catalogCard);
+        */
 	}
 
 }
