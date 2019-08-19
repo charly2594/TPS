21c96d6d63ca1a4c13f89876c8c09ad098f404a2
charly2594 <antinopai2594@gmail.com>
2019-08-19 09:53:29 -0700

    Commit of card catalog service Added methods: cardSearch cardFuzzySearch IdToImageURL IdToName
    CatalogCard class updated attributes and getter/setters

--- src/main/java/io/tps/yugioh/cardcatalogservice/models/CatalogCard.java
+++ src/main/java/io/tps/yugioh/cardcatalogservice/models/CatalogCard.java
@@ -10,12 +10,16 @@ public class CatalogCard {
 	private String id;
 	private String desc;
 	private String image_url;
+	private String image_url_small;
+	private LinkedHashMap card_prices;
 
     public CatalogCard(LinkedHashMap rawCardData){
         this.name = (String) rawCardData.get("name");
         this.id = (String) rawCardData.get("id");
         this.desc = (String) rawCardData.get("desc");
         this.image_url = (String) ((LinkedHashMap) ((List) rawCardData.get("card_images")).get(0)).get("image_url");
+        this.image_url_small = (String) ((LinkedHashMap) ((List) rawCardData.get("card_images")).get(0)).get("image_url_small");
+        this.card_prices = (LinkedHashMap) rawCardData.get("card_prices");
     }
 
 	public CatalogCard(String name, String desc, String id, String image_url) {
@@ -54,5 +58,21 @@ public class CatalogCard {
 
     public void setImage_url(String image_url) {
         this.image_url = image_url;
+    }
+
+    public String getImage_url_small() {
+        return image_url_small;
+    }
+
+    public void setImage_url_small(String image_url_small) {
+        this.image_url_small = image_url_small;
+    }
+
+    public LinkedHashMap getCard_prices() {
+        return card_prices;
+    }
+
+    public void setCard_prices(LinkedHashMap card_prices) {
+        this.card_prices = card_prices;
     }
 }
--- src/main/java/io/tps/yugioh/cardcatalogservice/resources/CardCatalogResource.java
+++ src/main/java/io/tps/yugioh/cardcatalogservice/resources/CardCatalogResource.java
@@ -13,17 +13,17 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("/catalog")
 public class CardCatalogResource {
-	private final RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
 
-	public CardCatalogResource(RestTemplateBuilder restTemplateBuilder) {
+    public CardCatalogResource(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-	@RequestMapping("/{cardName}")
-	public Object testPlayground(@PathVariable("cardName") String cardName){
+    @RequestMapping("/{cardName}")
+    public Object testPlayground(@PathVariable("cardName") String cardName) {
 
-        LinkedHashMap[] arrayResponse =  this.restTemplate.getForObject("https://db.ygoprodeck.com/api/v5/cardinfo.php?fname={cardName}", LinkedHashMap[].class, cardName);
+        LinkedHashMap[] arrayResponse = this.restTemplate.getForObject("https://db.ygoprodeck.com/api/v5/cardinfo.php?fname={cardName}", LinkedHashMap[].class, cardName);
         /*
         System.out.println(arrayResponse.getClass());
         System.out.println(arrayResponse[1].getClass());
@@ -33,23 +33,12 @@ public class CardCatalogResource {
         CatalogCard cardInstance = new CatalogCard(arrayResponse[1]);
         System.out.println(cardInstance);
         return arrayResponse[1];
-	}
-    @RequestMapping("/search={cardName}")
-    public List<CatalogCard> cardSearch(@PathVariable("cardName") String cardName){
-	    /*
-        //db.yugioh API request:
-        LinkedHashMap[] rawCardDataList =  this.restTemplate.getForObject("https://db.ygoprodeck.com/api/v5/cardinfo.php?fname={cardName}", LinkedHashMap[].class, cardName);
+    }
 
-        //card object instances:
-        List<CatalogCard> cardList = new LinkedList<CatalogCard>();
-        for (LinkedHashMap rawCardData : rawCardDataList){
-            CatalogCard cardInstance = new CatalogCard(rawCardData);
-            cardList.add(cardInstance);
-            //System.out.println(cardInstance.getImage_url());
-        }
-        return cardList;
-        */
+    @RequestMapping("/fsearch={cardName}")
+    public List<CatalogCard> cardFuzzySearch(@PathVariable("cardName") String cardName) {
 
+        // db.yugioh API request fuzzy search:
         List<CatalogCard> cardList = new LinkedList<CatalogCard>();
         try {
             RestTemplate restTemplate = new RestTemplate();
@@ -58,17 +47,17 @@ public class CardCatalogResource {
             headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
             HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 
-            String url_api_request = "https://db.ygoprodeck.com/api/v5/cardinfo.php?fname="+cardName;
-            ResponseEntity<Object> responseEntity = restTemplate.exchange(url_api_request, HttpMethod.GET,entity,Object.class);
+            String url_api_request = "https://db.ygoprodeck.com/api/v5/cardinfo.php?fname=" + cardName;
+            ResponseEntity<Object> responseEntity = restTemplate.exchange(url_api_request, HttpMethod.GET, entity, Object.class);
 
             System.out.println(responseEntity.getClass());
             System.out.println(responseEntity.getBody().getClass());
-            ArrayList<LinkedHashMap> rawCardDataList = (ArrayList<LinkedHashMap> ) responseEntity.getBody();
+            ArrayList<LinkedHashMap> rawCardDataList = (ArrayList<LinkedHashMap>) responseEntity.getBody();
 
 
             //card object instances:
 
-            for (LinkedHashMap rawCardData : rawCardDataList){
+            for (LinkedHashMap rawCardData : rawCardDataList) {
                 CatalogCard cardInstance = new CatalogCard(rawCardData);
                 cardList.add(cardInstance);
                 //System.out.println(cardInstance.getImage_url());
@@ -83,8 +72,40 @@ public class CardCatalogResource {
 
 
     }
+    @RequestMapping("/search={cardNameOrId}")
+    public CatalogCard cardSearch(@PathVariable("cardNameOrId") String cardName) {
+
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+            headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+            HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+            String url_api_request = "https://db.ygoprodeck.com/api/v5/cardinfo.php?name=" + cardName;
+            ResponseEntity<Object> responseEntity = restTemplate.exchange(url_api_request, HttpMethod.GET, entity, Object.class);
+            LinkedHashMap rawCardDataList = (LinkedHashMap) ( (ArrayList) responseEntity.getBody()).get(0);
 
 
+            //card object instances:
+            CatalogCard cardInstance = new CatalogCard(rawCardDataList);
+            return cardInstance;
 
-	}
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
 
+
+    }
+    @RequestMapping("/IdToImageURL={Id}")
+    public String IdToImageURL(@PathVariable("Id") String Id){
+        CatalogCard catalogCard = this.cardSearch(Id);
+        return catalogCard.getImage_url();
+    }
+    @RequestMapping("/IdToName={Id}")
+    public String IdToName(@PathVariable("Id") String Id){
+        CatalogCard catalogCard = this.cardSearch(Id);
+        return catalogCard.getName();
+    }
+}
